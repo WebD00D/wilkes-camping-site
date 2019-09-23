c89d031e9b387f9c4f47ee7349a81bd064a61f7d
Christian <christian@stabmag.com>
2019-09-22 17:53:20 -0700

    mapbox

--- src/components/Hook.js
+++ src/components/Hook.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 
 const MyHook = () => {
   const [count, setCount] = useState(0);
-const [otherCount, setOtherCount] = useState(10);
+  const [otherCount, setOtherCount] = useState(10);
 
   return (
     <div>
       <h1>{count}</h1>
       <h2>{otherCount}</h2>
-      <button onClick={() => setCount(count + 1) }>Add one</button>
+      <button onClick={() => setCount(count + 1)}>Add one</button>
     </div>
   );
 };
--- src/components/Mapbox.js
+++ src/components/Mapbox.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import ReactMapboxGl, { Layer, Feature, Marker } from 'react-mapbox-gl';
 import { MAPBOX_ACCESS_TOKEN } from '../constants/index';
 
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 
 const Mark = styled.div`
   background-color: red;
   height: 40px;
   width: 40px;
   border-radius: 50px;
-`
+`;
 
 const Map = ReactMapboxGl({
   accessToken: MAPBOX_ACCESS_TOKEN
@@ -18,13 +18,36 @@ const Map = ReactMapboxGl({
 export default class Mapbox extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      userLocation: [null, null], // this be provided from search, or geo location.
+      campsites: [] // array of array's of campsite locations..
+    };
+  }
+
+  renderCampsiteMarkers() {
+    const { campsites } = this.state;
+    const campsiteMarkers = Object.keys(campsites).map(k => {
+      const campsite = campsites[k];
+      return (
+        <Marker
+          coordinates={[campsite.latitude, campsite.longitude]}
+          anchor="bottom"
+        >
+          <Mark />
+        </Marker>
+      );
+    });
+
+    return campsiteMarkers;
   }
 
   render() {
     return (
       <div>
         <Map
+        latitude={51.3233379650232}
+          longitude={-0.481747846041145}
+          
           style="mapbox://styles/mapbox/streets-v9"
           containerStyle={{
             height: '500px',
@@ -37,10 +60,7 @@ export default class Mapbox extends Component {
             layout={{ 'icon-image': 'marker-15' }}
           >
             {/* <Feature coordinates={[-0.481747846041145, 51.3233379650232]} /> */}
-
-            <Marker coordinates={[37.5247764,-77.5633018]} anchor="bottom">
-              <Mark />
-            </Marker>
+            {this.renderCampsiteMarkers()}
           </Layer>
         </Map>
       </div>
