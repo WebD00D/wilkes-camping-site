cd6aae4db1655b3861057611a20776b1c000a274
Christian <christian@stabmag.com>
2019-09-29 12:32:23 -0700

    pushing mapbox updates

--- src/components/Mapbox.js
+++ src/components/Mapbox.js
@@ -1,45 +1,45 @@
-import React, { Component } from "react";
-import ReactMapboxGL from "react-map-gl";
-import { MAPBOX_ACCESS_TOKEN } from "../constants/index";
-import * as UI from "../UI";
-import styled from "@emotion/styled";
-import { fromJS } from "immutable";
+import React, { Component } from 'react';
+import ReactMapboxGL, { Marker } from 'react-map-gl';
+import { MAPBOX_ACCESS_TOKEN } from '../constants/index';
+import * as UI from '../UI';
+import styled from '@emotion/styled';
+import { fromJS } from 'immutable';
 
-// const Mark = styled.div`
-//   background-color: red;
-//   height: 40px;
-//   width: 40px;
-//   border-radius: 50px;
-// `;
+const Mark = styled.div`
+  background-color: red;
+  height: 40px;
+  width: 40px;
+  border-radius: 50px;
+`;
 
-// const mapStyle = fromJS({
-//   version: 8,
-//   sources: {
-//     points: {
-//       type: "geojson",
-//       data: {
-//         type: "FeatureCollection",
-//         features: [
-//           {
-//             type: "Feature",
-//             geometry: { type: "Point", coordinates: [-122.45, 37.78] }
-//           }
-//         ]
-//       }
-//     }
-//   },
-//   layers: [
-//     {
-//       id: "my-layer",
-//       type: "circle",
-//       source: "points",
-//       paint: {
-//         "circle-color": "#f00",
-//         "circle-radius": 40
-//       }
-//     }
-//   ]
-// });
+const mapStyle = fromJS({
+  version: 8,
+  sources: {
+    points: {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            geometry: { type: 'Point', coordinates: [-122.45, 37.78] }
+          }
+        ]
+      }
+    }
+  },
+  layers: [
+    {
+      id: 'my-layer',
+      type: 'circle',
+      source: 'points',
+      paint: {
+        'circle-color': '#f00',
+        'circle-radius': 40
+      }
+    }
+  ]
+});
 
 export default class Mapbox extends Component {
   constructor(props) {
@@ -49,41 +49,71 @@ export default class Mapbox extends Component {
       campsites: [], // array of array's of campsite locations..
 
       viewport: {
-        width: "800px",
-        height: "600px",
-        latitude: 37.7577,
-        longitude: -122.4376,
-        zoom: 8
+        latitude: 37.785164,
+        longitude: -100,
+        zoom: 3.5,
+        bearing: 0,
+        pitch: 0
       }
     };
   }
 
-  // renderCampsiteMarkers() {
-  //   const { campsites } = this.state;
-  //   const campsiteMarkers = Object.keys(campsites).map(k => {
-  //     const campsite = campsites[k];
-  //     return (
-  //       <Marker
-  //         coordinates={[campsite.latitude, campsite.longitude]}
-  //         anchor="bottom"
-  //       >
-  //         <Mark />
-  //       </Marker>
-  //     );
-  //   });
 
-  //   return campsiteMarkers;
-  // }
+
+  renderDataPoints() {
+    const CAMPSITES = [
+      { lat: 37.78, long: -122.45, text: '1' },
+      { lat: 38.78, long: -125.45,  text: '2' },
+      { lat: 39.78, long: -121.45, text: '3' }
+    ];
+
+    return Object.keys(CAMPSITES).map(k => {
+    
+      const campsite = CAMPSITES[k]
+      return (
+        <Marker
+          key={`marker-${k}`}
+          longitude={campsite.long}
+          latitude={campsite.lat}
+        >
+          <Mark />
+          {campsite.text}
+        </Marker>
+      );
+    });
+  }
 
   render() {
+    console.log('campsite markers', this.state.campsites);
+
+    const sources = {
+      points: {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [
+            {
+              type: 'Feature',
+              geometry: { type: 'Point', coordinates: [-122.45, 37.78] }
+            }
+          ]
+        }
+      }
+    };
+
     return (
       <div>
         <ReactMapboxGL
-          mapStyle={"mapbox://styles/mapbox/outdoors-v10"}
-          mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
           {...this.state.viewport}
+          sources={sources}
+          mapStyle={'mapbox://styles/mapbox/outdoors-v10'}
+          mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+          width="800px"
+          height="600px"
           onViewportChange={viewport => this.setState({ viewport })}
-        />
+        >
+          {this.renderDataPoints()}
+        </ReactMapboxGL>
       </div>
     );
   }
