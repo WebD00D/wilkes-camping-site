4c4c8b12157212ee139cec19f8628e502975e797
boothaw <andersonwilkesbooth@gmail.com>
2019-09-29 19:40:50 -0700

    styled markers

--- src/components/Mapbox.js
+++ src/components/Mapbox.js
@@ -1,116 +1,74 @@
-import React, { Component } from 'react';
-import ReactMapboxGL, { Marker } from 'react-map-gl';
-import { MAPBOX_ACCESS_TOKEN } from '../constants/index';
-import * as UI from '../UI';
-import styled from '@emotion/styled';
-import { fromJS } from 'immutable';
-
-const Mark = styled.div`
-  background-color: red;
-  height: 40px;
-  width: 40px;
-  border-radius: 50px;
-`;
-
-const mapStyle = fromJS({
-  version: 8,
-  sources: {
-    points: {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: [
-          {
-            type: 'Feature',
-            geometry: { type: 'Point', coordinates: [-122.45, 37.78] }
-          }
-        ]
-      }
-    }
-  },
-  layers: [
-    {
-      id: 'my-layer',
-      type: 'circle',
-      source: 'points',
-      paint: {
-        'circle-color': '#f00',
-        'circle-radius': 40
-      }
-    }
-  ]
-});
+import React, { Component } from "react";
+import ReactMapboxGL, { Marker } from "react-map-gl";
+import CampPin from "../UI/CampPin";
+import { MAPBOX_ACCESS_TOKEN } from "../constants/index";
+import * as UI from "../UI";
+import styled from "@emotion/styled";
+import { fromJS } from "immutable";
 
 export default class Mapbox extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mapStyle: "mapbox://styles/mapbox/outdoors-v10",
+
       userLocation: [null, null], // this be provided from search, or geo location.
       campsites: [], // array of array's of campsite locations..
 
       viewport: {
         latitude: 37.785164,
-        longitude: -100,
-        zoom: 3.5,
+        longitude: -122,
+        zoom: 6,
         bearing: 0,
         pitch: 0
       }
     };
   }
 
-
-
   renderDataPoints() {
     const CAMPSITES = [
-      { id: 123, lat: 37.78, long: -122.45, text: '1' },
-      { id: 456, lat: 38.78, long: -125.45,  text: '2' },
-      { id: 678, lat: 39.78, long: -121.45, text: '3' }
+      { id: 123, lat: 37.78, long: -122.45, text: "1" },
+      { id: 456, lat: 38.78, long: -125.45, text: "2" },
+      { id: 678, lat: 39.78, long: -121.45, text: "3" }
     ];
 
     return Object.keys(CAMPSITES).map(k => {
-    
-      const campsite = CAMPSITES[k]
+      const campsite = CAMPSITES[k];
       return (
         <Marker
           key={`marker-${k}`}
           longitude={campsite.long}
           latitude={campsite.lat}
         >
-          <a href={`/campsite/${campsite.id}`}><Mark /></a>
+          <a href={`/campsite/${campsite.id}`}>
+            <CampPin />
+          </a>
           {campsite.text}
         </Marker>
       );
     });
   }
 
-  render() {
-    console.log('campsite markers', this.state.campsites);
+  onViewportChange(viewport) {
+    this.setState({ viewport });
+  }
+  _onStyleChange(mapStyle) {
+    this.setState({ mapStyle });
+  }
 
-    const sources = {
-      points: {
-        type: 'geojson',
-        data: {
-          type: 'FeatureCollection',
-          features: [
-            {
-              type: 'Feature',
-              geometry: { type: 'Point', coordinates: [-122.45, 37.78] }
-            }
-          ]
-        }
-      }
-    };
+  render() {
+    // console.log("campsite markers", this.state.campsites);
+    const { viewport, mapStyle } = this.state;
 
     return (
       <div>
         <ReactMapboxGL
-          {...this.state.viewport}
-          sources={sources}
-          mapStyle={'mapbox://styles/mapbox/outdoors-v10'}
+          {...viewport}
+          mapStyle={mapStyle}
+          onViewportChange={this._onViewportChange}
           mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
           width="800px"
           height="600px"
-          onViewportChange={viewport => this.setState({ viewport })}
         >
           {this.renderDataPoints()}
         </ReactMapboxGL>
